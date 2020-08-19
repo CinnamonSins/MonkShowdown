884b7e93c63afc8d409dcaf1846928bc287c686b
CinnamonSins <69937796+CinnamonSins@users.noreply.github.com>
2020-08-19 18:32:52 -0400

    I brought in all of my files.
    This is the first thing, it's pretty great.

---
+++ Assets/Scripts/MKController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MKController : MonoBehaviour
+{
+    // Start is called before the first frame update,
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        Vector2 position = transform.position;
+        position.x = position.x + 3f * horizontal*Time.deltaTime;
+        position.y = position.y + 3f * vertical*Time.deltaTime    ;
+        transform.position = position;
+    }
+}
